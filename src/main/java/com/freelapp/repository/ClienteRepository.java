9d67538088ac094659e93289d829ae6e6688f716
FrancescoDAmbrosio1 <158843577+FrancescoDAmbrosio1@users.noreply.github.com>
2024-11-05 23:51:28 +0100

    Ampliata ricerca a tutti i campi del cliente

--- src/main/java/com/freelapp/repository/ClienteRepository.java
+++ src/main/java/com/freelapp/repository/ClienteRepository.java
@@ -13,7 +13,14 @@ public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
     
     
 //    query custom per la ricerca per partita IVA
-    @Query("SELECT c FROM Cliente c WHERE c.partitaIva LIKE '%'||:input||'%' ")
+//    @Query("SELECT c FROM Cliente c WHERE c.partitaIva LIKE '%'||:input||'%' ")
 	
+    @Query("SELECT c FROM Cliente c WHERE c.name LIKE '%'||:input||'%' OR "
+    		+ "c.ragioneSociale LIKE '%'||:input||'%' OR "
+    		+ "c.email LIKE '%'||:input||'%' OR "
+    		+ "c.telefono LIKE '%'||:input||'%' OR "
+    		+ "c.indirizzo LIKE '%'||:input||'%' OR "
+    		+ "c.city LIKE '%'||:input||'%' OR "
+    		+ "c.partitaIva LIKE '%'||:input||'%' ")
     public List<Cliente> search( String input);
 }
