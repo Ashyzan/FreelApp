884062dbeb7c366535c4d7a87f2d2250187534ed
RosaCielo <ashyzan@gmail.com>
2025-01-10 13:30:40 +0100

    updated task stato rif as nullable

--- src/main/java/com/freelapp/model/Task.java
+++ src/main/java/com/freelapp/model/Task.java
@@ -1,9 +1,9 @@
 package com.freelapp.model;
 
 import java.time.LocalDate;
-import java.util.List;
 
 import org.springframework.format.annotation.DateTimeFormat;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -12,11 +12,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull; 
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 @Entity
@@ -63,7 +62,7 @@ public class Task{
 	private Progetto progetto;
 	
 	@ManyToOne
-	@JoinColumn(name = "StatoRif", nullable = false)
+	@JoinColumn(name = "StatoRif", nullable = true)
 	private Stato stato;
 	
 	public int getId() {
