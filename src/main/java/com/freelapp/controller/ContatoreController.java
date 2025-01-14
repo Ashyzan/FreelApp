6351ce5a6f3709b278544695b02bb0c3d11da476
RosaCielo <ashyzan@gmail.com>
2025-01-14 15:40:04 +0100

    Contatore and task controller (#31)
    * refactoring of contatore - intermediate update
    
    * commit
    
    * commit
    
    * Commit: Start Contatore works
    
    * Contatore update
    
    aggiunto record (di tipo string) in tabella contatore per calcolare il tempo trascorso fra start e pause, implementato metodi per calcolare e registrare a DB il numero delle pause, salvare a db lo start e lo stop.
    
    * cotatore stop updated
    
    added conditions for start, stop pause
    
    * Contatore completed!

--- src/main/java/com/freelapp/controller/ContatoreController.java
+++ src/main/java/com/freelapp/controller/ContatoreController.java
@@ -3,15 +3,13 @@ package com.freelapp.controller;
 import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import jakarta.validation.Valid;
+
 import com.freelapp.model.Contatore;
 import com.freelapp.model.Task;
 import com.freelapp.repository.ContatoreRepository;
@@ -30,92 +28,114 @@ private ContatoreRepository repositContatore;
 @GetMapping("/Contatore/timer/{id}")
 public String gestioneTimer(@PathVariable("id") Integer taskId, Model model) {
 	
-	boolean b;
-	Task t = repositTask.getReferenceById(taskId);
+        // richiamo l'id del task 
+	Task task = repositTask.getReferenceById(taskId);
 	
-	if (t.getContatore() == null) {
-	
-			Contatore contatore = new Contatore();
-	
-			contatore.setTask(t);
-	
-			System.out.println("indice del task: " + contatore.getTask().getId()); 
-
-			b = true;
-			model.addAttribute("contatore", contatore);
-			model.addAttribute("b", b);
-
-			return "/Contatore/timer";
-	   }
-	else {
-		    Contatore contatore = t.getContatore();
-		    
-		    b = false;
-			model.addAttribute("contatore", contatore);
-			model.addAttribute("b", b);
-
-			return "/Contatore/timer";
-	}
+	return "/Contatore/timer";
  }
 
-//@PostMapping("/Contatore/timer/{id}")
-//public String gestioneTimer( @Valid @ModelAttribute("contatore") Contatore contatore, BindingResult bindingResult, Model model) {
-		
-	//if(bindingResult.hasErrors()) {
-	  //   return "/Contatore/timer";
-	  //}
-	
-//	System.out.println("indice del task" + contatore.getTask().getId()); 
-	
-	//repositContatore.save(contatore);
-	
-	//return "redirect:/Contatore/timer/" + contatore.getTask().getId();
-	
-  //}
 
-
-@PostMapping("/Contatore/start")
-public String startContatore(@ModelAttribute("contatore") Contatore contatore, @ModelAttribute("b") boolean b, Model model)
+@PostMapping("/start/{id}")
+public String startContatore(@PathVariable("id") Integer taskId, 
+	@ModelAttribute("contatore") Contatore contatore, Model model)
 {		
-	//contatore.setStart(LocalDateTime.now());
+ 
+    	// richiamo l'id del task
+    	Task task = repositTask.getReferenceById(taskId);
+    	
+    	// se il contatore esiste, restituiscilo al modello
+    	if (task.getContatore() != null) {
+  
+    	// collego nel modello html il task e il contatore
+    	    model.addAttribute("task" , task);
+    	    model.addAttribute("contatore" , contatore);
+    	}
+    	
+    	// se il contatore non esiste:
+    	else  {
+    	    
+    	    // istanzio un nuovo contatore
+    	    contatore = new Contatore();
+    	    
+    	    // associo al task il nuovo contatore
+    	    task.setContatore(contatore);
+    	    
+    	    //eseguo il TIMESTAMP
+    	    contatore.setStart(LocalDateTime.now());
+    	    
+    	    // collego nel modello html il task e il contatore
+    	    model.addAttribute("task" , task);
+    	    model.addAttribute("contatore" , contatore);
+    	    
+    	    // salvo il contatore a DB
+    	    repositContatore.save(contatore);
+    	}
+    	    
+    
+	return "/Contatore/timer";
 	
-	System.out.println("indice del task in start" + contatore.getTask().getId()); 
-	
-	if (b)
-	   repositContatore.updateStart(LocalDateTime.now(), contatore.getId());
-	
-	//repositContatore.save(contatore);
-	
-	return "redirect:/dashboard";
 }
 
 
-@PostMapping("/Contatore/pause")
-public String pauseContatore(@ModelAttribute("contatore") Contatore contatore, Model model)
+@PostMapping("/Contatore/pause/{id}")
+public String pauseContatore(@PathVariable("id") Integer taskId)
 {
-	//contatore.setPause(LocalDateTime.now());
+    
+ // richiamo l'id del task
+	Task task = repositTask.getReferenceById(taskId);
 	
-	contatore.addStop_numbers();
+	// verifica che il contatore esista
+	if (task.getContatore() != null) {
+	    
+	 task.getContatore().setPause(LocalDateTime.now());
+	    
+	    // recupero timestamp di inizio e pausa
+	    LocalDateTime PAUSE = task.getContatore().getPause();
+	    LocalDateTime START = task.getContatore().getStart();
+	    
+	    // metodo che calcola la differenza fra i due timestamp
+	    String FinalTime = task.getContatore().findDifference(START, PAUSE);
+	    
+	    // ad ogni clic la funzione prende gli stop e incrementa di 1
+	    task.getContatore().setStop_numbers(task.getContatore().getStop_numbers() + 1);
+	    
+	    // salvo il contatore
+	    repositContatore.save(task.getContatore());
+	    
+	}
 	
-	repositContatore.updatePause(LocalDateTime.now(), contatore.getId());
-	
-	//repositContatore.save(contatore);
-	
-	System.out.println(contatore.getStop_numbers());
-	
-	return "redirect:/dashboard";
+	return "/Contatore/timer";
 }
 
 
-@PostMapping("/Contatore/stop")
-public String stopContatore(@ModelAttribute("contatore") Contatore contatore, Model model)
+@PostMapping("/Contatore/stop/{id}")
+public String stopContatore(@PathVariable("id") Integer taskId)
 {
-	//contatore.setStop(LocalDateTime.now());
-
-	repositContatore.updateStop(LocalDateTime.now(), contatore.getId());
-	//repositContatore.save(contatore);
-	
-	return "redirect:/dashboard";
+ // richiamo l'id del task
+ 	Task task = repositTask.getReferenceById(taskId);
+ 	
+ 	// verifica che il contatore esista
+ 	if (task.getContatore() != null) {
+ 	    
+ 	    // verifica che il contatore sia ancora attivo
+ 	    if(task.getContatore().getStop() == null) {
+ 		
+ 		//eseguo il TIMESTAMP dello STOP
+ 		task.getContatore().setStop(LocalDateTime.now());
+ 		// recupero timestamp di start e stop
+ 	    }
+ 	    LocalDateTime STOP = task.getContatore().getStop();
+ 	    LocalDateTime START = task.getContatore().getStart();
+ 	    
+ 	    // metodo che calcola la differenza fra i due timestamp
+ 	    String FinalTime = task.getContatore().findDifference(START, STOP);
+ 	    
+ 	    // salvo il contatore
+ 	    repositContatore.save(task.getContatore());
+ 	}
+		
+    	
+	return "/Contatore/timer";
  }
 
 }
