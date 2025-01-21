68a3b2577361e3a6c0565e61294f308c5561375f
Lucia Di Bello <lucia.dibello3@gmail.com>
2025-01-21 23:58:30 +0100

    Update timer.js

--- src/main/resources/static/js/timer.js
+++ src/main/resources/static/js/timer.js
@@ -6,37 +6,51 @@ let seconds = 0;
 let minutes = 0;
 let hours = 0;
 
-function start() {
-	 interval = setInterval(timer, 1000); 
- }
- 
- const pause = () => {
-      clearInterval(interval);
- }
+if (localStorage.getItem("seconds") != 0) 
+     seconds = localStorage.getItem("seconds");
 
- const stop = () => {
+if (localStorage.getItem("minutes") != 0) 
+     minutes = localStorage.getItem("minutes");
+     
+if (localStorage.getItem("hours") != 0) 
+     hours = localStorage.getItem("hours");
+     
+function start() {
+ interval = setInterval(timer, 1000); 
+}
+
+const pause = () => {
      clearInterval(interval);
-  }
+}
+
+const stop = () => {
+    clearInterval(interval);
+ }
  
 const digitZero = (digit) => {
-    if (digit < 10)
-        return `0${digit}`;
-    else return digit;
+   if (digit < 10)
+       return `0${digit}`;
+   else return digit;
 }
 
 function timer() {
-    seconds++;
+   seconds++;
 
-    if (seconds == 60) {
-          minutes++;
-          seconds = 0;  
-    }
+   if (seconds == 60) {
+         minutes++;
+         seconds = 0;  
+   }
 
-    if (minutes == 60) {
-          minutes = 0;
-          hours++;
-    }
+   if (minutes == 60) {
+         minutes = 0;
+         hours++;
+   }
 
-    timerElement.innerHTML = digitZero(hours) + ":" + digitZero(minutes) + ":" + digitZero(seconds);
-  }
+   localStorage.setItem('hours', hours);
+   localStorage.setItem('minutes', minutes);
+   localStorage.setItem('seconds', seconds);
+  
+   timerElement.innerHTML = digitZero(localStorage.getItem("hours")) + ":" + digitZero(localStorage.getItem("minutes")) + ":" + digitZero(localStorage.getItem("seconds"));
+
+ }
 
