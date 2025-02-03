80a210ee580412fa92c94446fe3b1eaff0fcc949
RosaCielo <ashyzan@gmail.com>
2025-02-03 17:01:17 +0100

    commit
    clean code

--- src/main/resources/static/js/timer.js
+++ src/main/resources/static/js/timer.js
@@ -3,32 +3,29 @@ let minutes = (finalTime % 3600) / 60;
 let seconds = finalTime % 60;
 
 const timerElement = document.querySelector("#timer");
-			
+
 function tempochescorre() {
-
-	let tempochescorre = finalTime + timenow;
-	let hours = tempochescorre / 3600;
-	let minutes = (tempochescorre % 3600) / 60;
-	let seconds = tempochescorre % 60;
-	
 	seconds++;
+	stampacontatore();
 
-	if (seconds == 60) {
-		minutes++;
+	if (seconds >= 60) {
 		seconds = 0;
+		minutes++;
 	}
 
-	if (minutes == 60) {
+	if (minutes >= 60) {
 		minutes = 0;
 		hours++;
 	}
-	stampacontatore();
+
 
 }
 
 function stampacontatore() {
 
+	// formattato con 2 cifre, per difetto
 	document.getElementById('timer').innerHTML = ('0' + Math.floor(hours)).slice(-2) + ":" + ('0' + Math.floor(minutes)).slice(-2) + ":" + ('0' + Math.floor(seconds)).slice(-2);
+
 
 }
 
@@ -36,24 +33,26 @@ function timerstart() {
 
 
 	if (contatoreTrue && contatoreIsRun) {
+		// eseguo prim la funzione una volta per togliere il lag di 1 secondo, poi entro nel ciclo
+		tempochescorre();
 		setInterval(tempochescorre, 1000);
 
 		console.log("isRun !!");
 		console.log("finaltime is " + finalTime);
+		console.log("ore " + hours + " minuti " + minutes + " secondi " + seconds)
 		console.log("timenow is " + timenow)
 
 	}
 
-	else if (contatoreTrue) {
+	else if (contatoreIsRun !== true) {
 
-		tempochescorre();
 		stampacontatore();
-		console.log(finalTime);
+		console.log("contatore non attivo , finaltime = " + finalTime);
 	}
 
 	else {
 
-		console.log("finaltime is not defined" + finalTime);
+		console.log("finaltime is not defined , finaltime = " + finalTime);
 	}
 
 }
