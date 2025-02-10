c6aa3992c7429d752daf684bfc7b95d6dd049a09
RosaCielo <ashyzan@gmail.com>
2025-02-10 18:29:04 +0100

    8766 time limit - commit
    Aggiunto condizione che stoppa il timer se eccede le dimensioni consentite (8766 ore cioè 365 giorni) while inserito nel servicecontatore.

--- src/main/java/com/freelapp/controller/ContatoreController.java
+++ src/main/java/com/freelapp/controller/ContatoreController.java
@@ -1,10 +1,12 @@
 package com.freelapp.controller;
 
 import java.time.LocalDateTime;
-
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -161,6 +163,9 @@ public class ContatoreController {
 
 	// CONTATORE IN START
 	else {
+		
+		// Appena avvi un nuovo timer metti in pausa eventuali timer attivi
+		
 
 	    // istanzio un nuovo contatore
 	    contatore = new Contatore();
--- src/main/java/com/freelapp/service/ContatoreService.java
+++ src/main/java/com/freelapp/service/ContatoreService.java
@@ -3,81 +3,102 @@ package com.freelapp.service;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 
 
 import com.freelapp.model.Task;
 
+
 @Service
 public class ContatoreService {
+	
 
+	// verifica che il contatore esista
+	public void contatoreIsTrue(Task task, Model model) {
 
-    // verifica che il contatore esista
-    public void contatoreIsTrue(Task task, Model model) {
+		Boolean contatoreIsTrue = false;
 
-	Boolean contatoreIsTrue = false;
+		// // se il contatore esiste
+		if (task.getContatore().getId() != 0) {
 
-	// // se il contatore esiste
-	if (task.getContatore().getId() != 0) {
+			contatoreIsTrue = true;
 
-	    contatoreIsTrue = true;
+			// model.addAttribute("finaltime", task.getContatore().getFinaltime());
+			model.addAttribute("contatoreIsTrue", contatoreIsTrue);
+		}
 
-	    // model.addAttribute("finaltime", task.getContatore().getFinaltime());
-	    model.addAttribute("contatoreIsTrue", contatoreIsTrue);
 	}
-
-    }
 
 // verifica ache il contatore stia andando cioè sia attivo
-    public Boolean contatoreIsRun(Task task, Model model) {
-	LocalDateTime START = task.getContatore().getStart();
-	LocalDateTime STOP = task.getContatore().getStop();
-	LocalDateTime RESTART = task.getContatore().getRestart();
-	LocalDateTime PAUSE = task.getContatore().getPause();
-	Boolean contatoreIsRun = false;
+	public Boolean contatoreIsRun(Task task, Model model) {
+		LocalDateTime START = task.getContatore().getStart();
+		LocalDateTime STOP = task.getContatore().getStop();
+		LocalDateTime RESTART = task.getContatore().getRestart();
+		LocalDateTime PAUSE = task.getContatore().getPause();
+		Boolean contatoreIsRun = false;
+		
 
-	// CASO 1 : RUN - CONTATORE SENZA PAUSE
-	if ((PAUSE == null) && (STOP == null)) {
-	    contatoreIsRun = true;
-	    model.addAttribute("contatoreIsRun", contatoreIsRun);
+		// CASO 1 : RUN - CONTATORE SENZA PAUSE
+		if ((PAUSE == null) && (STOP == null)) {
+			contatoreIsRun = true;
+			model.addAttribute("contatoreIsRun", contatoreIsRun);
 
+		}
+		// CASO 2 : RUN - CONTATORE RIAVVIATO
+		else if ((RESTART != null) && (RESTART.isAfter(PAUSE))) {
+			contatoreIsRun = true;
+			model.addAttribute("contatoreIsRun", contatoreIsRun);
+
+		}
+		// CASO 3: STOP - CONTATORE FERMO START E STOP
+		else if ((PAUSE != null) && (PAUSE.isAfter(START)) || (STOP != null)) {
+			contatoreIsRun = false;
+			model.addAttribute("contatoreIsRun", contatoreIsRun);
+		}
+
+		// CASO 4: STOP - CONTATORE FERMO RESTART E STOP
+		else if ((PAUSE != null) && (PAUSE.isAfter(RESTART)) || (STOP != null)) {
+			contatoreIsRun = false;
+			model.addAttribute("contatoreIsRun", contatoreIsRun);
+		}
+
+		return contatoreIsRun;
 	}
-	// CASO 2 : RUN - CONTATORE RIAVVIATO
-	else if ((RESTART != null) && (RESTART.isAfter(PAUSE))) {
-	    contatoreIsRun = true;
-	    model.addAttribute("contatoreIsRun", contatoreIsRun);
-
-	}
-	// CASO 3: STOP - CONTATORE FERMO START E STOP
-	else if ((PAUSE != null) && (PAUSE.isAfter(START)) || (STOP != null)) {
-	    contatoreIsRun = false;
-	    model.addAttribute("contatoreIsRun", contatoreIsRun);
-	}
-
-	// CASO 4: STOP - CONTATORE FERMO RESTART E STOP
-	else if ((PAUSE != null) && (PAUSE.isAfter(RESTART)) || (STOP != null)) {
-	    contatoreIsRun = false;
-	    model.addAttribute("contatoreIsRun", contatoreIsRun);
-	}
-
-	return contatoreIsRun;
-    }
 
 // function time difference start e stop
-    public Long findTime(LocalDateTime start_date, LocalDateTime end_date) {
+	public Long findTime(LocalDateTime start_date, LocalDateTime end_date) {
 
-	Long FinalTimeSeconds = start_date.until(end_date, ChronoUnit.SECONDS);
+		Long FinalTimeSeconds = start_date.until(end_date, ChronoUnit.SECONDS);
 
-	// Long hours = FinalTimeSeconds / 3600;
-	// Long minutes = (FinalTimeSeconds % 3600) / 60;
-	// Long seconds = FinalTimeSeconds1 % 60;
-	// finalTime = String.format("%02d:%02d:%02d", hours, minutes, seconds);
+		// Long hours = FinalTimeSeconds / 3600;
+		// Long minutes = (FinalTimeSeconds % 3600) / 60;
+		// Long seconds = FinalTimeSeconds1 % 60;
+		// finalTime = String.format("%02d:%02d:%02d", hours, minutes, seconds);
 
-	return FinalTimeSeconds;
-    }
+		return FinalTimeSeconds;
+	}
+	
+	
+//	public boolean timeExeed(LocalDateTime start_date, LocalDateTime end_date) {
+//	Long FinalTimeHours = START.until(PAUSE, ChronoUnit.HOURS);
+//	Long test = START.until(PAUSE, ChronoUnit.SECONDS);
+//
+//	// controlla che il timer non ecceda le ore consentite altrimenti mette in stop
+//	// il timer (8766 sono le ore presenti in un anno)
+//	if (
+//					test >= 35; 
+//			//FinalTimeHours >= 8766
+//			) {
+//		// errore: il task eccede le dimensioni consentite. Avviane uno nuovo.
+//		
+//		task.getContatore().setStop(LocalDateTime.now());
+//		repositContatore.save(task.getContatore());
+//		 
+//		bindingresult.addError(new ObjectError("ERRORE max hours.", "Il task eccede le dimensioni consentite ed è stato messo in stop, Avviane uno nuovo per continuare."));
+//		
+//		}
+//	}
 
- 
-    
 }
