7f6457ee148ff7729c73c8a84c21b33541949df7
RosaCielo <ashyzan@gmail.com>
2025-05-19 23:33:03 +0200

    donut config (partial)

--- src/main/resources/static/js/charts.js
+++ src/main/resources/static/js/charts.js
@@ -19,6 +19,45 @@ async function getProjectsArchived(){
 		
 		}) 
 
+		// donuts
+		const data = {
+		  labels: [
+		    'Red',
+		    'Blue',
+		    'Yellow'
+		  ],
+		  datasets: [{
+		    label: 'My First Dataset',
+		    data: [300, 50, 100],
+		    backgroundColor: [
+		      'rgb(255, 99, 132)',
+		      'rgb(54, 162, 235)',
+		      'rgb(255, 205, 86)'
+		    ],
+		    hoverOffset: 4
+		  }]
+		};
+		
+		const donut = document.getElementById('donut');
+					
+		new Chart(donut, {
+		  type: 'doughnut',
+		  data: data,
+		  options: {
+		    responsive: true,
+		    plugins: {
+		      legend: {
+		        position: 'top',
+		      },
+		      title: {
+		        display: true,
+		        text: 'Chart.js Doughnut Chart'
+		      }
+		    }
+		  },
+		});
+		
+		
 //creazione grafico
 	  const ctx = document.getElementById('myChart');
 
