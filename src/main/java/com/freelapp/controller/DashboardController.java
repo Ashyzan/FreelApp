77c5fd05372ddd95cd3c0a7b0bdb265558553e8a
FrancescoDAmbrosio1 <158843577+FrancescoDAmbrosio1@users.noreply.github.com>
2025-06-11 15:00:28 +0200

    risolto bug api-selectMode rapid button

--- src/main/java/com/freelapp/controller/DashboardController.java
+++ src/main/java/com/freelapp/controller/DashboardController.java
@@ -73,50 +73,31 @@ public class DashboardController {
 		// inizializzo a false così che al refresh esegue animazione solo se era stato cliccato in precedenza
 		ContatoreController.contatoreCliccatoPreRefresh = false;
 
-		List<Cliente> clienteList = new ArrayList<Cliente> ();
-
-		clienteList = clienteRepository.findAll(Sort.by(Sort.Direction.ASC, "Name"));
-
-		model.addAttribute("clientiList", clienteList);
-
-		
-		
-		List<Progetto> progettiList = new ArrayList<Progetto> ();
-
-		progettiList = progettoRepository.findAll(Sort.by(Sort.Direction.ASC, "Name"));
-
-		model.addAttribute("progettiList", progettiList);
-		
-		
-		//passa al model la lista di tutti i task esclusi quelli chiusi
+		//passa al model la lista di tutti i task esclusi quelli chiusi - serve per la generazione del rapid button
 		List<Task> taskList = new ArrayList<Task> ();
 		taskList = taskRepository.findAllNotClosed();
 		model.addAttribute("taskList", taskList);
 		
 		
-//		// lista che restituisce i task per modale delle ore lavorate sulla dashboard
-//		List<Task> taskListOreLavorate = new ArrayList<Task> ();
-//		taskList = taskRepository.findAll(Sort.by(Sort.Direction.DESC, "Name"));
-//		taskList.forEach( task -> {
-//			
-//			
-//				if( task.getContatore() == null) {
-//				taskListOreLavorate.add(task);
-//				
-//						}
-//				
-//				else if( task.getContatore() != null) {
-//					Boolean contatoreAttivo;
-//					contatoreAttivo = contatoreservice.contatoreIsRun(task);
+		
+		
+//		List<Cliente> clienteList = new ArrayList<Cliente> ();
+//
+//		clienteList = clienteRepository.findAll(Sort.by(Sort.Direction.ASC, "Name"));
+//
+//		model.addAttribute("clientiList", clienteList);
+//
 //		
-//					if(task.getContatore().getStop() == null  && contatoreAttivo == false) {
-//					
-//							 taskListOreLavorate.add(task); 
-//									
-//							 }	
-//						}
-//			model.addAttribute("taskListOreLavorate", taskListOreLavorate);
-//				} );
+//		
+//		List<Progetto> progettiList = new ArrayList<Progetto> ();
+//
+//		progettiList = progettoRepository.findAll(Sort.by(Sort.Direction.ASC, "Name"));
+//
+//		model.addAttribute("progettiList", progettiList);
+//		
+//		
+		
+	
 
 		
 		return "freelApp-dashboard";
