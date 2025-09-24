f39bded4a27c892ef23a7fe1ef0746ab752689b9
FrancescoDAmbrosio1 <158843577+FrancescoDAmbrosio1@users.noreply.github.com>
2025-09-24 15:58:31 +0200

    avantamento layout dashboard
    inserimento card task/progetti/clienti recenti da perfezionare altezza

--- src/main/resources/static/js/layout.js
+++ src/main/resources/static/js/layout.js
@@ -7,13 +7,11 @@ let buttontest = document.getElementById('buttontest');
 
 
 
-
-
 // recupero elementi dal DOM per navBar	
-const navBar = document.querySelector('.nav-bar');
+const navBar = document.getElementById('navbar');
 const navOptionsMobile = document.querySelector('#options-button-mobile');
 const navOptionsDesktop = document.querySelector('.nav-option-desktop');
-  
+
 
 //recupero elemento dal DOM per animazione partenza contatore
 const dettaglioContatoreTop = document.getElementById('dettaglio-contatore-top');
@@ -26,22 +24,44 @@ const textAreaDescrizioneCinquecento = document.getElementById('textarea-cinquec
 const tectAreaCinquecentoError = document.getElementById('textarea-cinquecento-error');
 const contatoreCaratteriCinquecentoContainer = document.getElementById('contatore-caratteri-cinquecento-container')
 
+//recupero dal DOM elemento main-contianer per dinamicizzare h della main section di ogni template
+const mainContainerTemplate = document.getElementById('main-container')
+
+//vengono inizializzate variabili  fuori dalla funzione function regolazioneAltezzaColonneLayout perche poi viene aggiornata man mano che viene utilizzata per
+//l'adattamento della dimenzione delle colonne del dettaglio progetto
+let altezzaNavbar = navBar.offsetHeight;
+let altezzaWindow = window.innerHeight;
+let altezzaMainContainerTemplate = altezzaWindow - altezzaNavbar
+let altezzaPrecedenteMainContainerTemplate = altezzaMainContainerTemplate
+
+window.addEventListener('resize', regolazioneMainContainer);
 
 
-
+//*******  funzione che in tutti i template misura le dimensioni della navbar e calcola di conseguenza le dimensioni del main-container ********************
+function regolazioneMainContainer() {
+	altezzaNavbar = navBar.offsetHeight;
+	console.log("°° altezza navbar: " + altezzaNavbar)
+	altezzaWindow = window.innerHeight;
+	console.log("°° altezzaWindow: " + altezzaWindow)
+	altezzaMainContainerTemplate = altezzaWindow - altezzaNavbar
+	console.log("°° altezzaMainContainerTemplate: " + altezzaMainContainerTemplate)
+	mainContainerTemplate.classList.remove('h-[' + altezzaPrecedenteMainContainerTemplate + 'px]')
+	mainContainerTemplate.classList.add('h-[' + altezzaMainContainerTemplate + 'px]')
+	altezzaPrecedenteMainContainerTemplate = altezzaMainContainerTemplate;
+}
 
 
 // ********** funzioni per navBar *********************************************************************  		
-function onToggleMenu(e){
+function onToggleMenu(e) {
 	navBar.classList.toggle('top-[-100%]')
 	navBar.classList.toggle('top-14')
 }
 
-function onToggleOptionsMobile(e){
+function onToggleOptionsMobile(e) {
 	navOptionsMobile.classList.toggle('hidden')
 }
 
-function onToggleOptionsDesktop(e){
+function onToggleOptionsDesktop(e) {
 	navOptionsDesktop.classList.toggle('top-[-100%]')
 	navOptionsDesktop.classList.toggle('top-14')
 }
@@ -53,27 +73,27 @@ function onToggleOptionsDesktop(e){
 
 
 // ********* animazione avvio contatore ******************************************************************************
-function controlloContatoreAttivato(){
-	
-	if(contatoreAttivato == true){
-		if(window.innerWidth < 1024){
-			document.getElementById('bottom').scrollIntoView({ behavior: "smooth"});
+function controlloContatoreAttivato() {
+
+	if (contatoreAttivato == true) {
+		if (window.innerWidth < 1024) {
+			document.getElementById('bottom').scrollIntoView({ behavior: "smooth" });
 			//document.getElementById('main-section').scrollTop = document.getElementById('main-section').scrollHeight + 100;
 			setTimeout(() => {
-				dettaglioContatoreBottom.classList.add('animate_animated','animate__bounceIn');				  
+				dettaglioContatoreBottom.classList.add('animate_animated', 'animate__bounceIn');
 			}, 500);
 		}
-		dettaglioContatoreTop.classList.add('animate_animated','animate__bounceIn');
+		dettaglioContatoreTop.classList.add('animate_animated', 'animate__bounceIn');
 	}
 }
 
 
 
 //******************* animazione se contatore cliccato su play/pause prima del refresh in mobile  ************/
-function contatoreCliccatoPlayPauseInMobile(){
-	
-	if(contatoreCliccatoPreRefresh == true && window.innerWidth < 1024){
-		document.getElementById('bottom').scrollIntoView({ behavior: "smooth"})
+function contatoreCliccatoPlayPauseInMobile() {
+
+	if (contatoreCliccatoPreRefresh == true && window.innerWidth < 1024) {
+		document.getElementById('bottom').scrollIntoView({ behavior: "smooth" })
 	}
 }
 
@@ -84,31 +104,31 @@ function contatoreCliccatoPlayPauseInMobile(){
 
 
 //*******  funzione per il countdown di caratteri nelle textarea di max 500 caratteri ********************
-if((contatoreCaratteriCinquecento != null) && (textAreaDescrizioneCinquecento != null)
-		 && (tectAreaCinquecentoError!= null)){
+if ((contatoreCaratteriCinquecento != null) && (textAreaDescrizioneCinquecento != null)
+	&& (tectAreaCinquecentoError != null)) {
 
-		let textAreaCharsLength = textAreaDescrizioneCinquecento.value.length;
-		const maxChar = 500;
-		if(textAreaCharsLength != maxChar){
-			contatoreCaratteriCinquecento.innerHTML = maxChar - textAreaCharsLength;
-		}else {
-			contatoreCaratteriCinquecento.innerHTML = maxChar;			
+	let textAreaCharsLength = textAreaDescrizioneCinquecento.value.length;
+	const maxChar = 500;
+	if (textAreaCharsLength != maxChar) {
+		contatoreCaratteriCinquecento.innerHTML = maxChar - textAreaCharsLength;
+	} else {
+		contatoreCaratteriCinquecento.innerHTML = maxChar;
+	}
+
+	textAreaDescrizioneCinquecento.addEventListener('keyup', function() {
+
+		textAreaCharsLength = textAreaDescrizioneCinquecento.value.length;
+		if (textAreaCharsLength == maxChar) {
+			contatoreCaratteriCinquecentoContainer.classList.add('hidden');
+			tectAreaCinquecentoError.innerHTML = " Hai raggiunto il limite massimo di " + maxChar + " caratteri!";
+		} else {
+			let charRemained = maxChar - textAreaCharsLength;
+			contatoreCaratteriCinquecentoContainer.classList.remove('hidden');
+			contatoreCaratteriCinquecento.innerHTML = charRemained;
+			tectAreaCinquecentoError.innerHTML = "";
 		}
-			
-			textAreaDescrizioneCinquecento.addEventListener('keyup', function() {
-		  	
-			textAreaCharsLength = textAreaDescrizioneCinquecento.value.length;
-		  	if (textAreaCharsLength == maxChar){
-				contatoreCaratteriCinquecentoContainer.classList.add('hidden');
-				tectAreaCinquecentoError.innerHTML = " Hai raggiunto il limite massimo di " + maxChar + " caratteri!";
-		  	} else {
-		   		let charRemained = maxChar - textAreaCharsLength;
-				contatoreCaratteriCinquecentoContainer.classList.remove('hidden');
-		    	contatoreCaratteriCinquecento.innerHTML = charRemained;
-				tectAreaCinquecentoError.innerHTML = "";
-		  		}
-			});
-	
+	});
+
 }
 
 
